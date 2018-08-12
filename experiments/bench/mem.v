d66aa9f7c6a313c23822e8e92bd1ad2311627cb1
Andres Erbsen <andreser@mit.edu>
2018-08-12 11:22:05 -0400

    mem.v

---
+++ experiments/bench/mem.v
@@ -0,0 +1,24 @@
+(* /usr/bin/time sh -c 'ulimit -v 900000; coqc -noinit mem.v' *)
+Require Import Coq.Lists.List.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
+Goal list nat. Time let x := eval cbv in (List.repeat 1000 1000) in exact x. Time Qed.
